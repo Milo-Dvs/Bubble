542d4eb2e6057e0a357413c3dd0d9da0dafcd7e0
juanito peralo <peralo.juanito@gmail.com>
2023-05-26 21:15:11 -0500

    Commit Inicial

---
+++ BubbleModified/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BubbleModified
+{
+    partial class Burbuja
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAgregar = new Button();
+            btnOrdenar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtNumero = new TextBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(330, 93);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(75, 23);
+            btnAgregar.TabIndex = 0;
+            btnAgregar.Text = "button1";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnOrdenar
+            // 
+            btnOrdenar.Location = new Point(434, 93);
+            btnOrdenar.Name = "btnOrdenar";
+            btnOrdenar.Size = new Size(75, 23);
+            btnOrdenar.TabIndex = 1;
+            btnOrdenar.Text = "button2";
+            btnOrdenar.UseVisualStyleBackColor = true;
+            btnOrdenar.Click += btnOrdenar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(367, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(146, 94);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(100, 23);
+            txtNumero.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Location = new Point(90, 182);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(617, 191);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+            tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            tabPage1.AutoScroll = true;
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(609, 163);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Paint += tabPage1_Paint;
+            // 
+            // Burbuja
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Controls.Add(txtNumero);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnOrdenar);
+            Controls.Add(btnAgregar);
+            Name = "Burbuja";
+            Text = "Form1";
+            Load += Burbuja_Load;
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAgregar;
+        private Button btnOrdenar;
+        private Label label1;
+        private Label label2;
+        private TextBox txtNumero;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+    }
+}
